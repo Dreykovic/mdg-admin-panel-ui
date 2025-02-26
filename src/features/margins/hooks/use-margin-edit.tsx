aa56b58800294db52b005123cce1c0a10a805f13
Dreykovic <amonaaudrey16@gmail.com>
2025-02-26 17:28:53 +0000

    [dev] refactor: Create hooks for margins table and edit tables

---
+++ src/features/margins/hooks/use-margin-edit.tsx
@@ -0,0 +1,70 @@
+import { useDispatch } from 'react-redux';
+import { useEditMarginMutation } from '@/store/api-slice';
+import { showAlert } from '@/components/ui/alerts/alert-slice';
+import { AppDispatch } from '@/store';
+import { ApiResponse } from '@/types/api';
+import { MarginLevel } from '@/types/entity';
+import { FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+
+export const useMarginEdit = (handleClose: () => void) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [editMargin] = useEditMarginMutation();
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required('Name is required.')
+      .max(100, 'Name must be 100 characters or less.'),
+    margin: Yup.number().required('Margin is required.'),
+  });
+
+  const handleSubmit = async (
+    values: Partial<MarginLevel>,
+    { setSubmitting }: FormikHelpers<Partial<MarginLevel>>,
+  ) => {
+    try {
+      if (values) {
+        const data: Partial<MarginLevel> = {
+          id: values.id,
+          name: values.name,
+          margin: values.margin,
+        };
+
+        const response: ApiResponse<MarginLevel> = await editMargin(
+          data as MarginLevel,
+        ).unwrap();
+
+        if (response.success) {
+          dispatch(
+            showAlert({
+              title: 'Success!',
+              message: response.message,
+            }),
+          );
+        }
+
+        handleClose();
+      } else {
+        throw new Error('No Data Provided');
+      }
+    } catch (error) {
+      console.error(error);
+      dispatch(
+        showAlert({
+          title: 'Error!',
+          message:
+            'An error occurred during the submission ' +
+            (error as any)?.data?.error?.message,
+          success: false,
+        }),
+      );
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return {
+    validationSchema,
+    handleSubmit,
+  };
+};
---
+++ src/features/margins/hooks/use-margins.tsx
@@ -0,0 +1,67 @@
+import { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { showAlert } from '@/components/ui/alerts/alert-slice';
+import { AppDispatch } from '@/store';
+import { useDeleteMarginMutation } from '@/store/api-slice';
+import { MarginLevel } from '@/types/entity';
+
+export const useMargins = () => {
+  const [updateInitialValues, setUpdateInitialValues] =
+    useState<Partial<MarginLevel>>();
+  const [showDeleteItemModal, setShowDeleteItemModal] = useState(false);
+  const [showEditSupplierModal, setShowEditSupplierModal] = useState(false);
+  const [marginId, setMarginId] = useState<number>();
+  const dispatch = useDispatch<AppDispatch>();
+  const [deleteMargin, { isLoading }] = useDeleteMarginMutation();
+
+  const handleDeleteItemModalClose = () => setShowDeleteItemModal(false);
+  const handleDeleteItemModalShow = () => setShowDeleteItemModal(true);
+
+  const handleEditMarginModalClose = () => setShowEditSupplierModal(false);
+  const handleEditMarginModalShow = () => setShowEditSupplierModal(true);
+
+  const handleDeletion = useCallback(async () => {
+    try {
+      if (marginId) {
+        const response = await deleteMargin({ id: marginId }).unwrap();
+
+        if (response.success) {
+          dispatch(
+            showAlert({
+              title: 'Success!',
+              message: response.message,
+            }),
+          );
+        }
+      } else {
+        throw new Error('No data provided');
+      }
+    } catch (error) {
+      console.error(error);
+      dispatch(
+        showAlert({
+          title: 'Error!',
+          message: 'An error occurred during deletion' + JSON.stringify(error),
+          success: false,
+        }),
+      );
+    } finally {
+      handleDeleteItemModalClose();
+    }
+  }, [marginId, deleteMargin, dispatch]);
+
+  return {
+    updateInitialValues,
+    setUpdateInitialValues,
+    showDeleteItemModal,
+    handleDeleteItemModalClose,
+    handleDeleteItemModalShow,
+    showEditSupplierModal,
+    handleEditMarginModalClose,
+    handleEditMarginModalShow,
+    marginId,
+    setMarginId,
+    isLoading,
+    handleDeletion,
+  };
+};
